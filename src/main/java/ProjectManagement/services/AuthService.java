833b96d0669809c35961301811520727282823c8
itamarit <itamarit124812@gmail.com>
2022-12-21 12:18:43 +0200

    fix exceptions

--- src/main/java/ProjectManagement/services/AuthService.java
+++ src/main/java/ProjectManagement/services/AuthService.java
@@ -1,25 +1,15 @@
 package ProjectManagement.services;
 
-import ProjectManagement.entities.Board;
-import ProjectManagement.repositories.BoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class AuthService {
 
-    @Autowired
-    private BoardRepository boardRepository;
+    public void login(String email, String password) {
 
-    /**
-     * method that responsible for finding board by id
-     * @param id
-     * @return  board if the id exits
-     */
-    public Optional<Board> findById(int id) {
-        return boardRepository.getBoardById(id);
-//        return Optional.empty();
+
+
+
     }
 }
