62097a2c4cb8b33a540b89b429cd0e11c0bfd946
itamarit <itamarit124812@gmail.com>
2022-12-25 15:37:36 +0200

    fix task Controller and service

--- src/main/java/ProjectManagement/services/TaskService.java
+++ src/main/java/ProjectManagement/services/TaskService.java
@@ -23,56 +23,63 @@ public class TaskService {
     @Autowired
     private BoardRepository boardRepository;
 
-    public List<Task> filter(TaskFields filterFields){
+    public List<Task> filter(TaskFields filterFields) {
         Integer creator = filterFields.getCreator();
         String title = filterFields.getTitle();
         LocalDateTime dueDate = filterFields.getDueDate();
-        Integer importance =filterFields.getImportance();
-        String description= filterFields.getDescription();
-        Integer taskParentId= filterFields.getTaskParentId();
-        List<Task>  filteredTak =taskRepository.findAll().stream().collect(Collectors.toList());
-        if(creator > 0){
-            filteredTak =filteredTak.stream().filter(task -> task.getCreator() == creator).collect(Collectors.toList());
+        Integer importance = filterFields.getImportance();
+        String description = filterFields.getDescription();
+        Integer taskParentId = filterFields.getTaskParentId();
+        List<Task> filteredTak = taskRepository.findAll().stream().collect(Collectors.toList());
+        if (creator > 0) {
+            filteredTak = filteredTak.stream().filter(task -> task.getCreator() == creator).collect(Collectors.toList());
         }
-        if(description != null){
-            filteredTak =filteredTak.stream()
+        if (description != null) {
+            filteredTak = filteredTak.stream()
                     .filter(task -> Objects.equals(task.getDescription(), description)).collect(Collectors.toList());
         }
-        if(title != null){
-            filteredTak =filteredTak.stream()
+        if (title != null) {
+            filteredTak = filteredTak.stream()
                     .filter(task -> Objects.equals(task.getTitle(), title)).collect(Collectors.toList());
         }
 
-        if(dueDate != null){
+        if (dueDate != null) {
             filteredTak = filteredTak.stream()
                     .filter(task -> task.getDueDate() == dueDate).collect(Collectors.toList());
         }
-        if(importance > 0){
+        if (importance > 0) {
             filteredTak = filteredTak.stream()
                     .filter(task -> task.getImportance() == importance).collect(Collectors.toList());
         }
 
-        if(taskParentId > 0){
+        if (taskParentId > 0) {
             filteredTak = filteredTak.stream()
                     .filter(task -> task.getTaskParentId() == taskParentId).collect(Collectors.toList());
         }
         return filteredTak;
     }
 
-    public Response<Task> addTask(int boardId, int taskParentId, int assignedUserId, int importance, String title, String description){
-        Optional<Board> boardFromDB = boardRepository.findById(boardId);
-        if(boardFromDB.isPresent()) {
-            Board board = boardFromDB.get();
-            Task task = new Task(board, taskParentId, assignedUserId, importance, title, description);
-            Task savedTask = taskRepository.save(task);
-            return Response.createSuccessfulResponse(savedTask);
-        }
-        return Response.createFailureResponse("Board not found");
+    public Response<Task> addTask(Task task) {
+        Task savedTask = taskRepository.save(task);
+        return Response.createSuccessfulResponse(savedTask);
     }
 
     public List<Task> getAll() {
         return taskRepository.findAll();
     }
 
+    public Response<Task> getTask(int taskId) {
+        Optional<Task> task = taskRepository.findById(taskId);
+        if (task.isPresent()) {
+            return Response.createSuccessfulResponse(task.get());
+        } else {
+            return Response.createFailureResponse("task not found");
+        }
+    }
+
+    public Response<Void> deleteTask(int taskId) {
+        taskRepository.deleteById(taskId);
+        return Response.createSuccessfulResponse(null);
+    }
 
 }
